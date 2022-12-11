a4cbffaa944ced9c7efe6a5c9495dfa5f2376db8
Prit3496 <gandhiprit1695@gmail.com>
2022-12-11 09:06:07 -0500

    first commit

---
+++ src/main/java/com/Calculator/Calculator.java
@@ -0,0 +1,31 @@
+package com.Calculator;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		public int add(int a,int b) {
+			int c;
+			c=a+b;
+			return c;
+			}
+		public int sub(int a,int b){
+			int c;
+			c=a-b;
+			return c;
+		}
+		public int multi(int a, int b) {
+			int c;
+			c=a*b;
+			return c;
+		}
+		public int dev(int a, int b) {
+			int c;
+			c=a/b;
+			return c;
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
